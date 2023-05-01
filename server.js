d49eb102a511df2a31bc88feed0e2584d2683fb1
BranningK <bmk2110@gmail.com>
2023-05-01 10:59:16 -0400

    fixed server and updated routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 // Require model
-const { Library } = require('./models');
+const { User, Thought } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,16 +9,59 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Finds all books
-app.get('/all-books', (req, res) => {
+// Finds all Users
+app.get('/users', (req, res) => {
   // Using model in route to find all documents that are instances of that model
-  Library.find({}, (err, result) => {
+  User.find({}, (err, result) => {
     if (err) {
       res.status(500).json({ message: 'Internal server error' });
     }
     res.status(200).json(result);
   });
 });
+
+// Finds Single User
+app.get('/users/:id', (req, res) => {
+  console.log("Req Parameters: ", req.params);
+  
+  User.findById({ _id: req.params.id }, (err, data) => {
+    if(err) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+    res.status(200).json(data);
+    //res.status(200).json({ message: "Find Single User Route", data})
+  })
+  
+})
+
+// Create A New Users
+app.post('/users', (req, res) => {
+  console.log("Req Body: ", req.body);
+  
+  User.create(req.body, (err, data) => {
+    if(err) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
+   // res.status(201).json(data);
+    res.status(201).json({ message: "Create New User Route", data})
+  })
+  
+})
+
+// Find Single User and UPDATE 
+app.put('/users/:id', (req, res) => {
+  
+  console.log("Req Parameters: ", req.params);
+  console.log("Req Body: ", req.body);
+
+  res.status(200).json({ message: "Update Single User Route"})
+})
+// REmove USER from DB
+app.delete('/users/:id', (req, res) => {
+  console.log("Req Parameters: ", req.params);
+
+  res.status(200).json({ message: "Delete Single User Route"})
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
