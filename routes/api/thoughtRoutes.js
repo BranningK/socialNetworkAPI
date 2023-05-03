d73d11614d1b23f7060591f2472cb64d431640bb
Branning <bmk2110@gmail.com>
2023-05-03 17:18:49 -0400

    added thought route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,6 +5,7 @@ const {
   createThought,
   updateThought,
   deleteThought,
+  addReaction,
   // createReaction,
   // deleteReaction,
 } = require("../../controllers/thoughtController");
@@ -18,5 +19,10 @@ router.route("/").get(getAllThoughts).post(createThought);
 // DELETE to remove a thought by its _id
 router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought);
 
+// PUT to add a reaction to a thought by its _id
+router.route("/:id/reactions").put(addReaction);
+
+// DELETE to remove a reaction to a thought by its _id and its reactionId
+router.route("/:id/reactions/:reactionId");
 
 module.exports = router;
